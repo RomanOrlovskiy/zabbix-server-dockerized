8f7f67a982c220f0ee20a1bfdbfc74ffb50d5e10
Roman Orlvoskyi <romanorlovskiy92@gmail.com>
2019-05-12 16:25:09 +0300

    Init commit

---
+++ zabbix-agent-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+ZABBIX_SERVER="zabbix.quitequiet.net"
+
+if [ "$UID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Only run it if we can (ie. on Ubuntu/Debian)
+if [ -x /usr/bin/apt-get ]; then
+  apt-get update
+  apt-get -y install zabbix-agent
+  systemctl enable zabbix-agent
+  sed -i "s/Server=127.0.0.1/Server=$ZABBIX_SERVER/" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER/" /etc/zabbix/zabbix_agentd.conf
+  HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
+  service zabbix-agent restart
+fi
+
+# Only run it if we can (ie. on RHEL/CentOS)
+if [ -x /usr/bin/yum ]; then
+  yum -y update
+  rpm -ivh http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm
+  yum -y install zabbix-agent
+  chkconfig zabbix-agent on
+  sed -i "s/Server=127.0.0.1/Server=$ZABBIX_SERVER/" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER/" /etc/zabbix/zabbix_agentd.conf
+  HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
+  service zabbix-agent restart
+fi
